cfa192d06b2e9357d512efbd47f1867c8fa47664
Samantha brake <sammy.brake@gmail.com>
2018-09-27 01:53:38 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,11 @@
 class Dog
-  attr_reader :name 
-
-def initialize(name)
-  @name = name 
+ 
+ attr_reader :name 
+   attr_accessor :mood 
+  
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end 
+  
 end
